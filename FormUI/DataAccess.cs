ad0de71eb1291cfc8650d3b20f12358404bbbaa2
Declan Keogh <declankeogh01@gmail.com>
2017-11-23 10:03:54 +0100

    Add project files.

---
+++ FormUI/DataAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace FormUI
+{
+    public class DataAccess
+    {
+        public List<Person> GetPeople(string lastName)
+        {
+            //automatically closes sonnection after executing this line of code...
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("SampleDB")))
+            {
+                //var output = connection.Query<Person>($"select * from people where LastName = '{lastName}'").ToList();
+                var output = connection.Query<Person>("dbo.People_GetByLastName @LastName", new { LastName = lastName } ).ToList();
+                return output;
+            }
+        }
+
+        public void InsertPerson(string firstName, string lastName, string emailAddress, string phoneNumber)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("SampleDB")))
+            {
+                // Person newPerson = new Person { FirstName = firstName, LastName = lastName, EmailAddress = emailAddress, PhoneNumber = phoneNumber };
+                //  or...
+                // Person newPerson = new Person();
+                // newPerson.FirstName = firstName;
+                // newPerson.LastName = lastName;
+                // etc.
+
+                List<Person> people = new List<Person>();
+
+                people.Add(new Person { FirstName = firstName, LastName = lastName, EmailAddress = emailAddress, PhoneNumber = phoneNumber });
+
+                connection.Execute("dbo.People_Insert @FirstName, @LastName, @EmailAddress, @PhoneNumber", people);
+
+            }
+        }
+    }
+}
